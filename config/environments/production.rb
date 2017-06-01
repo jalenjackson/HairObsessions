72ed7863578fe4830c1cc82baafad2bb988cbc6f
Jalen Jackson <jalenjack201@hotmail.com>
2017-05-31 22:02:07 -0400

    paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,14 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
 
-
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+          :bucket => ENV['S3_BUCKET'],
+          :access_key_id => ENV['S3_ACCESS_KEY'],
+          :secret_access_key => ENV['S3_SECRET']
+      }
+  }
 
 
   # Compress JavaScripts and CSS.
